6beb7b32c1d816e08f06e68fd265ddb1931c5e99
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2018-01-06 00:22:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "route works"
+    erb :index
+  end
+
+  get '/new' do
+    "route works as well"
+    erb :create_puppy
+  end
+
+  post '/new' do
+    "This one as well"
+    dog_name = params[:name]
+    dog_breed = params[:breed]
+    dog_age = params[:age]
+    @new_dog = Puppy.new(dog_name, dog_breed, dog_age)
+    erb :display_puppy
+  end
 end
